62c17bbb47df886271a90b5c0c82b6dcd039be14
byBackfish <38255215+byBackfish@users.noreply.github.com>
2018-04-11 16:51:10 +0200

    Update secrets.java

--- LucCraft/src/main/java/util/secrets.java
+++ LucCraft/src/main/java/util/secrets.java
@@ -2,6 +2,6 @@ package util;
 
 public class secrets {
 
-    public static String Token = "NDMyOTcwOTA0Njk2Mzg5NjM0.Da1DTg.QbaTr4aHgkAUAh1PGbsFOBH2GkQ";
+    public static String Token = "censiored";
 
 }
